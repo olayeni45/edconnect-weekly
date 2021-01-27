53a1470629ec0bc43d8d97e1f3a883d199287d87
olayeni45 <olayeni45@gmail.com>
2021-01-27 15:13:44 +0100

    Completed Week7 Exercise

--- app/web/src/shared/Header.jsx
+++ app/web/src/shared/Header.jsx
@@ -22,30 +22,29 @@ export default () => {
         history.push("/");
     }
 
-    if (window.location.href.split("3000")[1] === "/") {
-        home = "/";
-        useEffect(() => {
 
-            if (document.cookie != "") {
-                uid = document.cookie.split(';').find(row => row.startsWith('uid')).split('=')[1];
-                let personalCookieUri = usersUri + "/" + uid;
+    useEffect(() => {
 
-                fetch(personalCookieUri,
-                    {
-                        "method": "GET"
-                    })
-                    .then(async (response) => {
-                        var data = await response.json();
-                        console.table(data)
-                        setWelcome(data.firstname);
-                    })
-                    .catch((error) => {
-                        console.log("Error: ", error);
-                    });
-            }
-        }, [])
+        if (document.cookie != "") {
+            uid = document.cookie.split(';').find(row => row.startsWith('uid')).split('=')[1];
+            let personalCookieUri = usersUri + "/" + uid;
 
-    }
+            fetch(personalCookieUri,
+                {
+                    "method": "GET"
+                })
+                .then(async (response) => {
+                    var data = await response.json();
+                    console.table(data)
+                    setWelcome(data.firstname);
+                })
+                .catch((error) => {
+                    console.log("Error: ", error);
+                });
+        }
+    }, [])
+
+
 
 
     return (
@@ -68,23 +67,17 @@ export default () => {
             </Nav>
 
 
-            {(home === "/" && (document.cookie != "")) ? (
+            {((document.cookie != "")) ? (
                 <Nav className="justify-content-end">
                     <Nav.Link href="" name="logout" onClick={handleLogout}>Logout</Nav.Link>
                     <Nav.Link href="" name="welcome">Hi, {welcome}</Nav.Link>
                 </Nav>
             )
-                : (sign == "true") ? (
-                    <Nav className="justify-content-end">
-                        <Nav.Link href="" ></Nav.Link>
-                        <Nav.Link href="" ></Nav.Link>
-                    </Nav>
-                )
-                    :
-                    (<Nav className="justify-content-end">
-                        <Nav.Link href="/signup" name="signup">Sign Up</Nav.Link>
-                        <Nav.Link href="/login" name="login">Login</Nav.Link>
-                    </Nav>)}
+                :
+                (<Nav className="justify-content-end">
+                    <Nav.Link href="/signup" name="signup">Sign Up</Nav.Link>
+                    <Nav.Link href="/login" name="login">Login</Nav.Link>
+                </Nav>)}
 
 
         </Navbar>
