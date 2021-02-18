0b7a3877038595740ae27ffa5d9d577222ac3be0
olayeni45 <olayeni45@gmail.com>
2021-02-18 20:39:42 +0100

    Completed Week 8 Exercise

--- app/server/controllers/home.js
+++ app/server/controllers/home.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const projects = require('../services/project').getAll();
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
 
     // add code to render the Home Component, and pass in the projects  
     // as a props
+    const projects = require('../services/project').getAll();
     const user = req.session.user;
     res.render('Home', { projects, user })
 });
